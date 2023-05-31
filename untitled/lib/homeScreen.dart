35092eb366253c879e237ad2398f65a4620a6b34
Isaias_Felix <133122614+isaiasFelixEstudos@users.noreply.github.com>
2023-05-31 01:52:00 -0300

    dfgdfyhdf

---
+++ untitled/lib/homeScreen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Home'),
+        ),
+        actions: [
+          PopupMenuButton(
+              icon: Icon(Icons.list),
+              onSelected: (value) {
+                if (value == '/cadastroUsuario') {
+                  Navigator.of(context).pushNamed('/cadastroUsuario');
+                }
+                if (value == '/teladelogin') {
+                  Navigator.of(context).pushNamed('/teladelogin');
+                } else if (value == '/home') {
+                  Navigator.popUntil(context, ModalRoute.withName('/home'));
+                }
+              },
+              itemBuilder: (context) => [
+                    PopupMenuItem(
+                      child: Text('Home'),
+                      value: '/home',
+                    ),
+                    PopupMenuItem(
+                      child: Text('Cadastrar usuário'),
+                      value: '/cadastroUsuario',
+                    ),
+                    PopupMenuItem(
+                      child: Text('Tela de login'),
+                      value: '/teladelogin',
+                    ),
+                  ])
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(40, 40, 40, 40),
+            child: Column(
+              children: [
+                Center(
+                  child: Image.network(
+                      'https://viverdeblog.com/wp-content/uploads/2017/04/como-escrever-um-livro-topo.png',width: 200, height: 100,),
+                ),
+                SizedBox(height: 10,),
+                Center(child: Text('App de Livros'))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ untitled/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/firebase_options.dart';
+import 'homeScreen.dart';
+import 'paginas/cadastroUsuario.dart';
+import 'paginas/telaDeLogin.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/home',
+    routes: {
+      '/home': (_) => HomeScreen(),
+      '/cadastroUsuario': (_) => PaginaDeCadastro(),
+      '/teladelogin': (_) => TelaDeLogin(),
+    },),);
+}
