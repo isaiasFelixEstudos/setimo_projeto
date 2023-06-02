b25daf577bbc11163b338b40a4a902820ecb9b16
Isaias Félix Machado de Alcantara <133122614+isaiasFelixEstudos@users.noreply.github.com>
2023-06-02 17:07:15 -0400

    hyhfjh

--- untitled/lib/paginas/subpaginas/cadastro_genero.dart
+++ untitled/lib/paginas/subpaginas/cadastro_genero.dart
@@ -1,50 +1,65 @@
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/widgets.dart';
 
 class CadastroGenero extends StatefulWidget {
-  const CadastroGenero({super.key});
+  const CadastroGenero({Key? key}) : super(key: key);
 
   @override
   State<CadastroGenero> createState() => _CadastroGeneroState();
 }
 
 class _CadastroGeneroState extends State<CadastroGenero> {
-  final _formkey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
   bool _formValido = false;
-  TextEditingController _campodescricao = TextEditingController();
-  TextEditingController _campoindicacaoidade = TextEditingController();
-  TextEditingController _campotitulo = TextEditingController();
+  TextEditingController _campoDescricao = TextEditingController();
+  TextEditingController _campoIndicacaoIdade = TextEditingController();
+  TextEditingController _campoTitulo = TextEditingController();
 
-  void _validacaoFormulario() {
-    _formkey.currentState?.validate();
+  void _validarFormulario() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      AddCollection();
+    }
   }
 
-  String _validarEntrada(String? mensagem) {
+  void AddCollection() {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    firestore.collection('generos').add({
+      'descricao': _campoDescricao.text,
+      'indicacaoIdade': _campoIndicacaoIdade.text,
+      'titulo': _campoTitulo.text,
+    }).then((value) {
+      // Cadastro realizado com sucesso
+      print('Informações cadastradas com sucesso!');
+    }).catchError((error) {
+      // Ocorreu um erro durante o cadastro
+      print('Erro ao cadastrar as informações: $error');
+    });
+  }
+
+  String? _validarEntrada(String? mensagem) {
     if (mensagem == null || mensagem.isEmpty) {
       return 'Preencha o campo';
-    } else {
-      return 'Campo preenchido';
     }
+    return null;
   }
 
   @override
   void initState() {
     super.initState();
-    _campodescricao.addListener(_validacaoFormulario);
-    _campoindicacaoidade.addListener(_validacaoFormulario);
-    _campotitulo.addListener(_validacaoFormulario);
+    _campoDescricao.addListener(_validarFormulario);
+    _campoIndicacaoIdade.addListener(_validarFormulario);
+    _campoTitulo.addListener(_validarFormulario);
   }
 
   @override
   void dispose() {
     super.dispose();
-    _campoindicacaoidade.removeListener(_validacaoFormulario);
-    _campodescricao.removeListener(_validacaoFormulario);
-    _campotitulo.removeListener(_validacaoFormulario);
+    _campoIndicacaoIdade.removeListener(_validarFormulario);
+    _campoDescricao.removeListener(_validarFormulario);
+    _campoTitulo.removeListener(_validarFormulario);
   }
 
   @override
@@ -53,60 +68,57 @@ class _CadastroGeneroState extends State<CadastroGenero> {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+            padding: const EdgeInsets.all(15),
             child: Column(
               children: [
-                Text('Área de Cadastro de Generos de Livros'),
-                SizedBox(
-                  height: 10,
+                Text('Área de Cadastro de Gêneros de Livros'),
+                SizedBox(height: 10),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _campoTitulo,
+                        decoration: InputDecoration(labelText: 'Título'),
+                        validator: _validarEntrada,
+                        onChanged: (value) {
+                          setState(() {
+                            _formValido = value.isNotEmpty;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 10),
+                      TextFormField(
+                        controller: _campoDescricao,
+                        decoration:
+                            InputDecoration(labelText: 'Descrição do gênero'),
+                        validator: _validarEntrada,
+                        onChanged: (value) {
+                          setState(() {
+                            _formValido = value.isNotEmpty;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 10),
+                      TextFormField(
+                        controller: _campoIndicacaoIdade,
+                        decoration:
+                            InputDecoration(labelText: 'Indicação de idade'),
+                        validator: _validarEntrada,
+                        onChanged: (value) {
+                          setState(() {
+                            _formValido = value.isNotEmpty;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-                TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  controller: _campotitulo,
-                  decoration: InputDecoration(labelText: 'Titulo'),
-                  onChanged: (value) {
-                    setState(
-                      () {
-                        _formValido = value.isNotEmpty;
-                      },
-                    );
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  controller: _campodescricao,
-                  decoration: InputDecoration(labelText: 'Descrição do genero'),
-                  onChanged: (value) {
-                    setState(
-                      () {
-                        _formValido = value.isNotEmpty;
-                      },
-                    );
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  controller: _campoindicacaoidade,
-                  decoration: InputDecoration(labelText: 'Indicação idade'),
-                  onChanged: (value) {
-                    setState(
-                      () {
-                        _formValido = value.isNotEmpty;
-                      },
-                    );
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
+                SizedBox(height: 10),
                 ElevatedButton(
-                    onPressed: () {}, child: Text('Cadastrar Genero'))
+                  onPressed: _validarFormulario,
+                  child: Text('Cadastrar Gênero'),
+                ),
               ],
             ),
           ),
