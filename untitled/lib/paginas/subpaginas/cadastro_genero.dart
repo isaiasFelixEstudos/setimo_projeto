d0adc1e4645d94037419de13c678b08ef68399a9
Isaias Félix Machado de Alcantara <133122614+isaiasFelixEstudos@users.noreply.github.com>
2023-06-02 15:35:01 -0400

    rtegergt

--- untitled/lib/paginas/subpaginas/cadastro_genero.dart
+++ untitled/lib/paginas/subpaginas/cadastro_genero.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/widgets.dart';
 
 class CadastroGenero extends StatefulWidget {
@@ -9,11 +10,111 @@ class CadastroGenero extends StatefulWidget {
 }
 
 class _CadastroGeneroState extends State<CadastroGenero> {
+  final _formkey = GlobalKey<FormState>();
+
+  bool _formValido = false;
+  TextEditingController _campodescricao = TextEditingController();
+  TextEditingController _campoindicacaoidade = TextEditingController();
+  TextEditingController _campotitulo = TextEditingController();
+
+  void _validacaoFormulario() {
+    _formkey.currentState?.validate();
+  }
+
+  /*
+  if (sigla.isNotEmpty && definicao.isNotEmpty) {
+      _db.collection('siglas').add({
+        'sigla': sigla,
+        'definicao': definicao,
+        'email': email,
+        'telefone': telefone,
+        'criacao': FieldValue.serverTimestamp(),
+      });
+  */
+
+  String _validarEntrada(String? mensagem) {
+    if (mensagem == null || mensagem.isEmpty) {
+      return 'Preencha o campo';
+    } else {
+      return 'Campo preenchido';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _campodescricao.addListener(_validacaoFormulario);
+    _campoindicacaoidade.addListener(_validacaoFormulario);
+    _campotitulo.addListener(_validacaoFormulario);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _campoindicacaoidade.removeListener(_validacaoFormulario);
+    _campodescricao.removeListener(_validacaoFormulario);
+    _campotitulo.removeListener(_validacaoFormulario);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+            child: Column(
+              children: [
+                Text('Área de Cadastro de Generos de Livros'),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  controller: _campotitulo,
+                  decoration: InputDecoration(labelText: 'Titulo'),
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        _formValido = value.isNotEmpty;
+                      },
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  controller: _campodescricao,
+                  decoration: InputDecoration(labelText: 'Descrição do genero'),
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        _formValido = value.isNotEmpty;
+                      },
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  controller: _campoindicacaoidade,
+                  decoration: InputDecoration(labelText: 'Indicação idade'),
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        _formValido = value.isNotEmpty;
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
