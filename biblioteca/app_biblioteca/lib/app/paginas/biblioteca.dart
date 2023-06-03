f0ea194ecac677262bfb0f1c5c8ab0cd1407b654
Isaias Félix Machado de Alcantara <133122614+isaiasFelixEstudos@users.noreply.github.com>
2023-06-03 12:05:40 -0400

    fgfdg

--- biblioteca/app_biblioteca/lib/app/paginas/biblioteca.dart
+++ biblioteca/app_biblioteca/lib/app/paginas/biblioteca.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Biblioteca extends StatefulWidget {
   const Biblioteca({super.key});
@@ -8,11 +9,28 @@ class Biblioteca extends StatefulWidget {
 }
 
 class _BibliotecaState extends State<Biblioteca> {
+  Future<void> getColecao() async {
+    final db = FirebaseFirestore.instance;
+    final generos = db.collection('generos').doc('comedia_');
+    generos.get().then((DocumentSnapshot doc) {
+      final data = doc.data() as Map<String, dynamic>;
+    }, onError: (e) => print('Erro'));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getColecao();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          ElevatedButton(onPressed: getColecao, child: Text('Listar generos'))
+        ],
       ),
     );
   }
