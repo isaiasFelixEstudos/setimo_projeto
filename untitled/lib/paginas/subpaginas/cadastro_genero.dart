ecc5611176fcc27065319cee5bf0f693eefabc41
Isaias Félix Machado de Alcantara <133122614+isaiasFelixEstudos@users.noreply.github.com>
2023-06-03 09:41:55 -0400

    dsfsdf

--- untitled/lib/paginas/subpaginas/cadastro_genero.dart
+++ untitled/lib/paginas/subpaginas/cadastro_genero.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CadastroGenero extends StatefulWidget {
   const CadastroGenero({super.key});
@@ -8,11 +9,28 @@ class CadastroGenero extends StatefulWidget {
 }
 
 class _CadastroGeneroState extends State<CadastroGenero> {
+  CollectionReference genero = FirebaseFirestore.instance.collection('generos');
+  Future<void> addCollection() async {
+    return genero.doc("comedia_romantica_").set({
+      'descricao': 'genero novo',
+      'indicacaoIdade': 'Livre',
+      'titulo': 'Comédia ROmantica'
+    });
+    /*return genero.add({
+      'descricao': 'genero novo',
+      'indicacaoIdade': 'Livre',
+      'titulo': 'Comédia ROmantica'
+    }).then((value) => print('Genero Adicionado'));*/
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          ElevatedButton(
+              onPressed: addCollection, child: Text('Adicionar coleção'))
+        ],
       ),
     );
   }
