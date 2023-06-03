55657c246e53332193abc3ab2c1b0d7c0bc7b044
Isaias Félix Machado de Alcantara <133122614+isaiasFelixEstudos@users.noreply.github.com>
2023-06-03 11:29:19 -0400

    ffgg

--- biblioteca/app_biblioteca/lib/app/paginas/biblioteca.dart
+++ biblioteca/app_biblioteca/lib/app/paginas/biblioteca.dart
@@ -17,3 +17,54 @@ class _BibliotecaState extends State<Biblioteca> {
     );
   }
 }
+/*
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Biblioteca extends StatefulWidget {
+  const Biblioteca({Key? key});
+
+  @override
+  State<Biblioteca> createState() => _BibliotecaState();
+}
+
+class _BibliotecaState extends State<Biblioteca> {
+  List<DocumentSnapshot> _dataList = [];
+  Future<void> _fetchData() async {
+    QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('generos').get();
+    setState(() {
+      _dataList = snapshot.docs;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: _dataList.length,
+          itemBuilder: (context, index) {
+            Map<String, dynamic> data =
+                _dataList[index].data() as Map<String, dynamic>;
+            String titulo = data['titulo'];
+            String descricao = data['descricao'];
+            return ListTile(
+              title: Text(titulo),
+              subtitle: Text(descricao),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+*/
--- biblioteca/app_biblioteca/lib/app/paginas/subpaginas/add_livro.dart
+++ biblioteca/app_biblioteca/lib/app/paginas/subpaginas/add_livro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AddLivro extends StatefulWidget {
   const AddLivro({super.key});
@@ -8,11 +9,26 @@ class AddLivro extends StatefulWidget {
 }
 
 class _AddLivroState extends State<AddLivro> {
+  final db = FirebaseFirestore.instance;
+  Future<void> AddLivro() async {
+    final livro = <String, dynamic>{
+      "descricao": "Genero teste",
+      "indicacaoIdade": "Livre",
+      "titulo": "genero teste"
+    };
+    db.collection('generos').add(livro).then(
+          (DocumentReference doc) =>
+              print('DocumentSnapshot added with ID: ${doc.id}'),
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          ElevatedButton(onPressed: AddLivro, child: Text('Adicionar Livro'))
+        ],
       ),
     );
   }
