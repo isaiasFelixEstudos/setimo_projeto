2b4342bfdccf484de7ea7c97b5e9a01d93cd4449
Isaias Félix Machado de Alcantara <133122614+isaiasFelixEstudos@users.noreply.github.com>
2023-06-10 11:45:30 -0400

    fdfdg

--- app_biblioteca/lib/app/updatecadUser.dart
+++ app_biblioteca/lib/app/updatecadUser.dart
@@ -49,70 +49,73 @@ class _UpdateCadUserState extends State<UpdateCadUser> {
     return Scaffold(
       appBar: CustomAppBar(title: 'Atualizar Cadastro de Usuário'),
       drawer: CustomDrawer(),
-      body: Column(
-        children: [
-          TextFormField(
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            controller: _emailvalido,
-            decoration: InputDecoration(
-              errorStyle:
-                  TextStyle(color: _formValido ? Colors.blue : Colors.red),
-              errorBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: _formValido ? Colors.blue : Colors.red)),
-              labelText: "Email",
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: _emailvalido,
+              decoration: InputDecoration(
+                errorStyle:
+                    TextStyle(color: _formValido ? Colors.blue : Colors.red),
+                errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: _formValido ? Colors.blue : Colors.red)),
+                labelText: "Email",
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _formValido = value.isNotEmpty;
+                });
+              },
+              validator: _validarEntrada,
             ),
-            onChanged: (value) {
-              setState(() {
-                _formValido = value.isNotEmpty;
-              });
-            },
-            validator: _validarEntrada,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            controller: _nomevalido,
-            decoration: InputDecoration(
-              errorStyle:
-                  TextStyle(color: _formValido ? Colors.blue : Colors.red),
-              errorBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: _formValido ? Colors.blue : Colors.red)),
-              labelText: "Nome",
+            SizedBox(
+              height: 20,
             ),
-            onChanged: (value) {
-              setState(() {
-                _formValido = value.isNotEmpty;
-              });
-            },
-            validator: _validarEntrada,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              if (_formkey.currentState!.validate()) {
-                atualizarUsuarioAuthFirebase(
-                    context, _emailvalido.text, _nomevalido.text);
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      Timer(Duration(seconds: 2), () {
-                        Navigator.of(context).pop();
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: _nomevalido,
+              decoration: InputDecoration(
+                errorStyle:
+                    TextStyle(color: _formValido ? Colors.blue : Colors.red),
+                errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: _formValido ? Colors.blue : Colors.red)),
+                labelText: "Nome",
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _formValido = value.isNotEmpty;
+                });
+              },
+              validator: _validarEntrada,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (_formkey.currentState!.validate()) {
+                  atualizarUsuarioAuthFirebase(
+                      context, _emailvalido.text, _nomevalido.text);
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        Timer(Duration(seconds: 2), () {
+                          Navigator.of(context).pop();
+                        });
+                        return AlertDialog(
+                          content: Text('Cadastro atualizado com sucesso'),
+                        );
                       });
-                      return AlertDialog(
-                        content: Text('Cadastro atualizado com sucesso'),
-                      );
-                    });
-              }
-            },
-            child: Text('Atualizar Cadastro'),
-          ),
-        ],
+                }
+              },
+              child: Text('Atualizar Cadastro'),
+            ),
+          ],
+        ),
       ),
     );
   }
